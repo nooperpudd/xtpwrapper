9a51c07f995146a96954c892c6408b3ca2102129
winton wang <365504029@qq.com>
2018-08-29 13:55:19 +0800

    f

--- xtpwrapper/cppheader/c_quote_wrapper.h
+++ xtpwrapper/cppheader/c_quote_wrapper.h
@@ -18,7 +18,7 @@ static inline int QuoteSpi_OnError(PyObject *, XTPRI *);
 
 static inline int QuoteSpi_OnSubMarketData(PyObject *, XTPST *, XTPRI *, bool);
 static inline int QuoteSpi_OnUnSubMarketData(PyObject *, XTPST *, XTPRI *, bool);
-static inline int QuoteSpi_OnDepthMarketData(PyObject*, XTPMD*, long long *, int, int, long long *, int, int);
+static inline int QuoteSpi_OnDepthMarketData(PyObject*, XTPMD*, long long[], int, int, long long[], int, int);
 
 static inline int QuoteSpi_OnSubOrderBook(PyObject *, XTPST *, XTPRI *, bool);
 static inline int QuoteSpi_OnUnSubOrderBook(PyObject *, XTPST *, XTPRI *, bool);
